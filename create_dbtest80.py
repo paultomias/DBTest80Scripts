9f5b49fc9c4b60fcc112f5f0295358f5a7cef883
lawson <lawson@PHLSFREL10.lpc.lawson.com>
2017-07-11 21:54:36 -0500

    Modified method of creating capfile

--- create_dbtest80.py
+++ create_dbtest80.py
@@ -8,17 +8,15 @@ sysdump_path = file_path + "/files/dbtest80.sysdump"
 editda_path = file_path + "/files/dbtest80.editda"
 
 #Read and parse the config file
-f = open(cfg_path, "r")
+cfg_file = open(cfg_path, "r")
 
-for line in f:
+for line in cfg_file:
 	match = re.search(r'^([\w]+)\s*=\s*([\w\\\-\@]+)', line)
 	if match:
-		key = match.group(1)
-		value  = match.group(2) 
-		#Put the entries in the dictionary
-		if key not in cfg_dict:
-			cfg_dict[key] = value
-f.close()
+		if match.group(1) not in cfg_dict:
+			cfg_dict[match.group(1)] = match.group(2)
+
+cfg_file.close()
 
 #Run sysload of dbtest80.sysdump
 print("Running sysload for " + cfg_dict["PRODLINE"] + "...\n")
@@ -28,36 +26,38 @@ subprocess.call(["sysload", "-efp", sysdump_path, cfg_dict["PRODLINE"]])
 capfile_path = os.environ["LAWDIR"] + "/" + cfg_dict["PRODLINE"] + "/" + cfg_dict["DBTYPE"].upper()
 try:
 	print("Creating capfile...")
-	f = open(capfile_path, "w")
 
-	f.write("DBSERVER=" + cfg_dict["DBSERVER"] + "\n")
-	f.write("DBNAME=" + cfg_dict["DBNAME"] + "\n")
-	f.write("SCHEMA=" + cfg_dict["SCHEMA"] + "\n")
+	cap_file = open(capfile_path, "w")
+	cfg_file = open(cfg_path, "r")
 
-	if "SERVICENAME" in cfg_dict:
-		f.write("SERVICENAME=" + cfg_dict["SERVICENAME"] + "\n")
-	else:
-		f.write("LOGINNAME=" + cfg_dict["LOGINNAME"] + "\n")
-		f.write("PASSWORD=" + cfg_dict["PASSWORD"] + "\n")
+	for line in cfg_file:
+		if 'PRODLINE' in line:
+			break
 
-	if cfg_dict["DBTYPE"].upper() == "MICROSOFT":
-		f.write("FILEGROUPS=" + cfg_dict["FILEGROUPS"] + "\n")
+		match = re.search(r'^([\w]+)\s*=\s*([\w\\\-\@]+)', line)
+		
+		if match:
+			cap_file.write(match.group()+"\n")
+
+	cap_file.close()
+	cfg_file.close()
 
 	print(cfg_dict["DBTYPE"].upper() + " capfile created in LAWDIR/" + cfg_dict["PRODLINE"] + " SUCCESSFULLY\n")
-	f.close()
+
 except OSError as e:
 	print ("Error: %s - %s." % (e.filename,e.strerror))
 
 #Create the editda config file based on the entries from the config file
 try:
 	print("Creating config file for editda command...")
-	f = open(editda_path, "w")
+	editda_file = open(editda_path, "w")
 
-	f.write("B " + cfg_dict["DBSPACE"] + " " + cfg_dict["DBTYPE"] + " " + cfg_dict["DATATABLESPACE"] + " " + cfg_dict["INDEXTABLESPACE"] + "\n")
-	f.write("D " + cfg_dict["PRODLINE"] + " " + cfg_dict["DBSPACE"])
+	editda_file.write("B " + cfg_dict["DBSPACE"] + " " + cfg_dict["DBTYPE"] + " " + cfg_dict["DATATABLESPACE"] + " " + cfg_dict["INDEXTABLESPACE"] + "\n")
+	editda_file.write("D " + cfg_dict["PRODLINE"] + " " + cfg_dict["DBSPACE"])
 
 	print("Config file created SUCCESSFULLY\n")
-	f.close()
+	editda_file.close()
+
 except OSError as e:
 	print ("Error: %s - %s." % (e.filename,e.strerror))
 
